b821aee8202f6ad3b14867954c28ac9b413642ea
Burhanuddin Mustafa Lakdawala <burhan123786@gmail.com>
2024-09-17 12:24:06 -0700

    trimps_player: add logging

--- trimps_player.js
+++ trimps_player.js
@@ -2,8 +2,10 @@ let curr_science = game.resources.science.owned;
 
 function switchToScience() {
     if (game.global.playerGathering !== "science" && (curr_science - game.resources.science.owned) > (curr_science * 0.01)) {
+		console.log((new Date()).toLocaleTimeString() + ' Gathering: Science');
         setGather('science');
         setTimeout(() => { 
+            console.log((new Date()).toLocaleTimeString() + ' Gathering: Metal');
             setGather('metal'); 
         }, 30*1000);
     }
